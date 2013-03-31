42f1bde9965ed137f8fdd0c17c745263ad7eea25
Devon Stewart <blast@hardchee.se>
2013-03-30 19:05:51 -0700

    Adding the first half of the play2 TodoList tutorial
    http://localhost:9000/@documentation/ScalaTodoList

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,8 +3,17 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import models._
+
+import play.api.data._
+import play.api.data.Forms._
+
 object Application extends Controller {
   
+  val taskForm = Form(
+    "label" -> nonEmptyText
+  )
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
@@ -12,4 +21,11 @@ object Application extends Controller {
   def hello(name: String) = Action {
     Ok(views.html.hello(name))
   }
+
+// Tasks
+  def tasks = Action {
+    Ok(views.html.task_list(Task.all(), taskForm))
+  }
+  def newTask = TODO
+  def deleteTask(id: Long) = TODO
 }
